6de3e4b1638d8823488e7b9df5243947b0ea600d
Suhel Ahmed <88050024+desiresuhel@users.noreply.github.com>
2021-10-12 00:10:02 -0400

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+var cacheName = 'cofessions-pwa';
+var filesToCache = [
+'/',
+'index.html',
+'style.css',
+'main.js'];
+
+self.addEventListener('install',function(e){
+	e.waitUntil(caches.open(cacheName).then(function(cache){
+		return cache.addAll(filesToCache);
+	})
+	);
+	self.skipWaiting();
+});
+
+self.addEventListener('fetch',function(e){
+	e.respondWith(caches.match(e.request).then(function(response){
+		return response || fetch(e.request);
+	})
+	);
+	
+});
+
